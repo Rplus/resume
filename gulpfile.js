a129292ac28e27d5baad08732eb08a12c4a323e2
Rplus <rplus.tw@gmail.com>
2014-12-07 03:43:56 +0800

    調整 autoprefixer 支援瀏覽器列表 主要加進 IE 9 & Opera presto 的 prefix

--- gulpfile.js
+++ gulpfile.js
@@ -29,12 +29,12 @@ var pagespeed = require('psi');
 var reload = browserSync.reload;
 
 var AUTOPREFIXER_BROWSERS = [
-  'ie >= 10',
+  'ie >= 9',
   'ie_mob >= 10',
   'ff >= 30',
   'chrome >= 34',
   'safari >= 7',
-  'opera >= 23',
+  'opera >= 12',
   'ios >= 7',
   'android >= 4.4',
   'bb >= 10'
