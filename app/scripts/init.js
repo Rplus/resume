be503985887763c049034e02ab6d2b2b555aa476
Rplus <rplus.tw@gmail.com>
2015-02-14 17:34:43 +0800

    init svgicon injection

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,6 +1,39 @@
 (function() {
   'use strict';
 
+  function ready(fn) {
+    if (document.readyState !== 'loading'){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+
+  var insertSvgSprite = function () {
+    var inlineSvg = localStorage.getItem('inlineSvg');
+
+    var injectSvg = function (svgContent) {
+        var div = document.createElement('div');
+        div.innerHTML = svgContent;
+        document.body.insertBefore(div, document.body.childNodes[0]);
+    };
+
+    if (inlineSvg) {
+      ready(function () {
+        injectSvg(inlineSvg);
+      });
+    } else {
+      var ajax = new XMLHttpRequest();
+      ajax.open('GET', './images/inject-svg/svgstore.svg', true);
+      ajax.send();
+      ajax.onload = function(e) {
+        var inlineSvgContent = ajax.responseText;
+        injectSvg(inlineSvgContent);
+        localStorage.setItem('inlineSvg', inlineSvgContent);
+      };
+    }
+  };
+
   (function modernizrInit () {
     var htmlClassName = localStorage.getItem('modernizrAllClass');
     var lastDetectTime = localStorage.getItem('modernizrLastDetectTime') * 1;
@@ -8,6 +41,7 @@
 
     if (htmlClassName && detectDuration < 259200000 ) { // 259200000 === 3 days
       document.documentElement.className = htmlClassName;
+      insertSvgSprite();
       return;
     }
 
@@ -17,6 +51,7 @@
     _injectJs.addEventListener('load', function () {
       localStorage.setItem('modernizrAllClass', document.documentElement.className);
       localStorage.setItem('modernizrLastDetectTime', new Date().getTime());
+      insertSvgSprite();
     });
     _head.appendChild(_injectJs);
   })();
