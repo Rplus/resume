022e5ddbf4780bc768c0f59cb32b424f3954673b
Rplus <rplus.tw@gmail.com>
2015-03-09 01:31:45 +0800

    避免 web-font 載入過久，文字遲遲無法出現的狀況， 這邊設定 1 秒的 timeout 即加上 .wf-slow class 來讓文字直接出現

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,11 +5,21 @@ RplusFns.ready(function () {
     var googleFontText = (googleFontUsed.textContent || googleFontUsed.innerText).trim();
     var googleFontURL = googleFontSource + '&text=' + encodeURIComponent(googleFontText);
 
+    // for web font load slowly, it'll add a class for showing the text
+    var webfontLoaderTimeout;
+
+    (function () {
+      webfontLoaderTimeout = setTimeout(function () {
+        googleFontUsed.className += ' wf-slow';
+      }, 1000);
+    })();
+
     var _link = document.createElement('link');
     _link.rel = 'stylesheet';
     _link.href = googleFontURL;
     _link.onload = function () {
       googleFontUsed.className += ' wf-loaded';
+      clearTimeout(webfontLoaderTimeout);
     };
     document.body.insertBefore(_link, document.body.childNodes[0]);
   })();
