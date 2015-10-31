3ad5f8db0f9815ed4b4bbd479b15aad04c033f9f
Rplus <rplus.tw@gmail.com>
2015-11-01 06:10:32 +0800

    修正 init js

--- 2015/app/scripts/_init.js
+++ 2015/app/scripts/_init.js
@@ -1,4 +1,4 @@
 'use strict';
 (function(_htmlTag) {
-  _htmlTag.className = _htmlTag.className.replace(/\bno-js\b/, '');
+  _htmlTag.className = _htmlTag.className.replace(/\bno-js\b/, ' js ');
 })(document.documentElement);
