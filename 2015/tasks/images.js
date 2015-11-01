cae4ca0effaec27ef1a0f7a3aa696c3341ec5fb9
Rplus <rplus.tw@gmail.com>
2015-11-01 22:41:00 +0800

    修正 jpg > webp 後 jpg 消失的問題

--- 2015/tasks/images.js
+++ 2015/tasks/images.js
@@ -8,6 +8,8 @@ module.exports = (gulp, $) => {
         progressive: true,
         interlaced: true
       })))
+      .pipe(gulp.dest('.tmp/images'))
+      .pipe(gulp.dest('dist/images'))
       .pipe($.if('*.jpg', $.webp()))
       .pipe(gulp.dest('.tmp/images'))
       .pipe(gulp.dest('dist/images'))
